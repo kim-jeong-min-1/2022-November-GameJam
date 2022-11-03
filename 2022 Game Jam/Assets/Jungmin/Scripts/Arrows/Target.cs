881396d156d9b07f635eaf7b0505bdb3ecb285eb
Jeongmin Kim <kjmgi0671@naver.com>
2022-11-03 15:34:37 +0900

    2022/11/03 Jeongmin

---
+++ 2022 Game Jam/Assets/Jungmin/Scripts/Arrows/Target.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private Sprite hitSprite;
+    public IEnumerator HitArrow()
+    {
+        var sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = hitSprite;
+        yield return new WaitForSeconds(0.5f);
+
+        while (sprite.color.a > 0)
+        {
+            Color color = sprite.color;
+            color.a -= 0.1f;
+
+            sprite.color = color;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        this.gameObject.SetActive(false);
+        yield return null;
+    }
+}
