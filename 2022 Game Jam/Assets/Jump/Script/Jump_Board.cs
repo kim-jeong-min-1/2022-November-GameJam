15c52b00230e2aaf940661653ed04755bb1f644f
SeungMIn <sdh220118@sdh.hs.kr>
2022-11-03 09:38:11 +0900

    Jump

--- 2022 Game Jam/Assets/Jump/Script/Jump_Board.cs
+++ 2022 Game Jam/Assets/Jump/Script/Jump_Board.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Jump_Board : MonoBehaviour
 {
 
+    Player Player;
+
+    public bool isOver = false;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(isOver == true) transform.DORotate(new Vector3(0, 0, 15), 2f, RotateMode.LocalAxisAdd);
+        else transform.DORotate(new Vector3(0, 0, -15), 2f, RotateMode.LocalAxisAdd);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Over_Check")
+        {
+            if (isOver == true) isOver = false;
+            else isOver = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.name == "Click_Check")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Player.Jump();
+            }
+        }
+    }
 }
